9a7c0bedfacf29251a88116c6c83af3f2bbbf6d9
Laura Buelte <laura.buelte@protonmail.com>
2026-06-23 14:41:02 +0200

    added test for substract_three

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,3 +6,9 @@ using Playground
     @test add_one(0) == 1
     @test add_one(-1) == 0
 end
+
+@testset "substract_three" begin
+    @test substract_three(1) == -2
+    @test substract_three(10) == 7
+    @test substract_three(3) == 0
+end
