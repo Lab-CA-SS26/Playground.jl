25068cfa33b6413d87ed5b8526ad4c2836bb1828
Aurora Rossi <aurora98rossi@gmail.com>
2026-06-23 14:18:16 +0200

    add double function tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,3 +6,9 @@ using Playground
     @test add_one(0) == 1
     @test add_one(-1) == 0
 end
+
+@testset "double" begin
+    @test double(1) == 2
+    @test double(0) == 0
+    @test double(-1) == -2
+end
