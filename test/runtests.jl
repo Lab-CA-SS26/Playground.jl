28b14980c214b0f2d6d2d21ee1af684749a98fd1
Ken Alan Berkpinar <ken.berkpinar@gmail.com>
2026-06-23 14:40:24 +0200

    write tests for isPi

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,3 +6,12 @@ using Playground
     @test add_one(0) == 1
     @test add_one(-1) == 0
 end
+
+@testset "isPi" begin
+    @test isPi(3.14159) == true
+    @test isPi(3.1411) == true
+    @test isPi(3.14) == false
+    @test isPi(3.15) == false
+    @test isPi(3.0) == false
+    @test isPi(4.0) == false
+end
